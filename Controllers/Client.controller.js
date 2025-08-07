82675621e78dc1d685cd91cda2950dcce3904b42
Swee87 <elijahog99@gmail.com>
2025-08-07 12:13:09 +0100

    updated client.controller

--- Controllers/Client.controller.js
+++ Controllers/Client.controller.js
@@ -1,4 +1,4 @@
-const { check, validationResult } = require('express-validator');
+const { validationResult } = require('express-validator');
 const Pricing = require('../models/PricingSchema');
 const { calculateDistance } = require('../utils/distanceCalculator');
 const redis = require('../Config/redis');
