115698fe0dfa0f4010bd3c131396d7714756c9ca
Swee87 <elijahog99@gmail.com>
2025-08-08 14:44:12 +0100

    updated  routes/SwaggerGoogleAuth.js

--- routes/SwaggerGoogleAuth.js
+++ routes/SwaggerGoogleAuth.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const passport = require('passport'); // Assuming you use passport-google-oauth20
+const passport = require('passport');
+const { rateLimit } = require('express-rate-limit');
 const router = express.Router();
 const {
   registrationValidation,
