adb7c1f6013c4984ea0ab877bb36d99d359db956
Swee87 <elijahog99@gmail.com>
2025-08-09 15:02:03 +0100

    updated routes routes/SwaggerGoogleAuth.js

--- routes/SwaggerGoogleAuth.js
+++ routes/SwaggerGoogleAuth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const { rateLimit } = require('express-rate-limit');
 const { body, check } = require('express-validator');
-
+const {verifyEmail, verifyDriverOtp, registerDriver, uploadImages} = require('../Controllers/Driver.controller.js');
 const {DriverLogin} = require('../Controllers/Driver.controller.js');
 const router = express.Router();
 const {
